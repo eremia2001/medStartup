a593e8c8d02696347e4a429001096f889e7f764f
Eremiya Rifat <eremia2021@gmail.com>
2023-11-25 21:26:29 +0100

    fixed next.config

--- medi-app/src/app/components/Button.tsx
+++ medi-app/src/app/components/Button.tsx
@@ -1,15 +1,13 @@
-import {ButtonProps} from '../types'
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-export default function Button({bgColor,title,className, handleBtnClick} : ButtonProps){
-    return (
-      <motion.button 
+export default function Button({ title, className, handleBtnClick }) {
+  return (
+    <motion.button
       onClick={handleBtnClick}
       className={`bg-secondary text-lg text-white rounded-lg px-3 py-2 ${className} w-[20vw] lg:[15vw] xl:w-[12vw]`}
-      whileHover={{scale:1.1}}
-      >
-        {title}
-      </motion.button>
-
-    )
-  }
+      whileHover={{ scale: 1.1 }}
+    >
+      {title}
+    </motion.button>
+  );
+}
